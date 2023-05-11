72f13ce0003659e99997d1dd934d160306cf2469
xiaojia <xiaojiao_12@163.com>
2023-05-11 17:39:26 +0800

    Site updated: 2023-05-11 17:39:24

--- js/lib/math.js
+++ js/lib/math.js
@@ -1,1 +1,1 @@
-mixins.math={created(){this.renderers.push(this.math)},methods:{math(){renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:true},{left:"$",right:"$",display:false},{left:"\\(",right:"\\)",display:false},{left:"\\[",right:"\\]",display:true}]})}}};
+mixins.math={created(){this.renderers.push(this.math)},methods:{math(){renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:!0},{left:"$",right:"$",display:!1},{left:"\\(",right:"\\)",display:!1},{left:"\\[",right:"\\]",display:!0}]})}}};
